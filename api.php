564c49d32b6416a7ba1721fbc966de7d3bd37216
swathidhamo <swathidhamo@gmail.com>
2017-12-16 15:15:36 +0530

    The fetch and book slot api are done

--- api.php
+++ api.php
@@ -1,51 +1,51 @@
 <html>
 <head>
-	<title></title>
-	<?php
-	require_once 'connect.php';
+    <title></title>
+    <?php
+    require_once 'connect.php';
 
-	//array to display the result
-	$response = array();
+    //array to display the result
+    $response = array();
     /*api call is symbolized by a GET parameter
      called 'apicall' */
     if(isset($_GET['apicall'])){
-    	switch($_GET['apicall']){
-    		case 'signup':
-    		//will be the register page
-    		if(checkIfAllParametersAreTrue(array('username', 'password','gender','mobile_number',
-    			'car_reg', 'car_size') ) ){
-    			require_once 'register.php';
-    		    if($result){
-    		    	$response['error'] = false; 
-				    $response['message'] = 'User registered successfully'; 
- 					$response['user'] = $username;
-    		    }
-    		}
-    		    else{
+        switch($_GET['apicall']){
+            case 'signup':
+            //will be the register page
+            if(checkIfAllParametersAreTrue(array('username', 'password','gender','mobile_number',
+                'car_reg', 'car_size') ) ){
+                require_once 'register.php';
+                if($result){
+                    $response['error'] = false; 
+                    $response['message'] = 'User registered successfully'; 
+                    $response['user'] = $username;
+                }
+            }
+                else{
                     $response['error'] = true; 
-				    $response['message'] = 'Parameters unavaliable'; 
-    		    } 
-    		
-    		break; 
-    		case 'login':
-    		//will be the login part
-    		if(checkIfAllParametersAreTrue(array('username','password'))){
-    			require_once 'login.php';
-    			if($rows==1){
-    				$response['error'] = false; 
-				    $response['message'] = 'User successfully logged in'; 
-				    $response['car_size'] = $password_hash; 
- 					$response['user'] = $username;
-    			}
-    			else{
-    				$response['error'] = true; 
-				    $response['message'] = 'User unsuccessful'; 
- 					
-    			}
-    		}
-    		break;
+                    $response['message'] = 'Parameters unavaliable'; 
+                } 
+            
+            break; 
+            case 'login':
+            //will be the login part
+            if(checkIfAllParametersAreTrue(array('username','password'))){
+                require_once 'login.php';
+                if($rows==1){
+                    $response['error'] = false; 
+                    $response['message'] = 'User successfully logged in'; 
+                    $response['car_size'] = $password_hash; 
+                    $response['user'] = $username;
+                }
+                else{
+                    $response['error'] = true; 
+                    $response['message'] = 'User unsuccessful'; 
+                    
+                }
+            }
+            break;
 
-            case 'fetch_slot':
+            case 'book_slot':
             if(checkIfAllParametersAreTrue(array('username'))){
                 //require_once 'check_slot.php';
                     $username = $_POST['username'];
@@ -61,13 +61,29 @@
                //  require_once 'fetch_slot.php';
 
                  $username = $_POST['username'];
-                 $query = "SELECT slot FROM mall WHERE max_size <= 1 ORDER BY max_size ASC LIMIT 1";
+                 $query = "SELECT slot FROM mall WHERE max_size <= 3 AND 
+                 occupied = 1 ORDER BY max_size ASC LIMIT 1";
                  $sql = mysqli_query($link,$query);  
                  $array = mysqli_fetch_assoc($sql);     
 
                   if($sql){
-                    $response['error'] = false; 
-                    $response['message'] = 'Slot fetched for your car is '.$array['slot'].' Have a good time !'; 
+                   
+                    date_default_timezone_set("Asia/Kolkata");
+                    $time = date('Y-m-d H:i:s'); 
+                   
+                    $update = "UPDATE mall SET username = '".$username."', 
+                    occupied = 2, start_time = '".$time."'   WHERE slot = '" .$array['slot']. "'"; 
+                    $query_update = mysqli_query($link, $update);
+                    if($query_update){
+                         $response['error'] = false; 
+                         $response['message'] = 'Slot fetched for your car is '.$array['slot'].' Have a good time !'; 
+                    }
+                    else{
+                          echo mysqli_error($link);
+                          $response['error'] = true; 
+                         $response['message'] = 'Cannot process request'; 
+                    }
+                    
                   }
                   else{
                     $response['error'] = true;
@@ -77,28 +93,56 @@
                   
                 }
             }
+
             break;
-    		default:
-    		$response['error'] = true; 
+            case 'fetch_slot':
+                if(checkIfAllParametersAreTrue(array('username'))){
+                //require_once 'check_slot.php';
+                    $username = $_POST['username'];
+                    $query = "SELECT slot FROM mall WHERE username = '".$username."' ";
+                    $sql = mysqli_query($link,$query);  
+                    $rows = mysqli_num_rows($sql);
+                    $array = mysqli_fetch_assoc($sql); 
+
+                if($rows>0){
+                   $response['error'] = false; 
+                   $response['message'] = 'Car is occupying slot '.$array['slot'].
+                   '  Thank you for visiting';  
+                }
+                else{
+                   $response['error'] = true; 
+                   $response['message'] = 'No slot found ';  
+
+                  
+                }
+            }
+
+            break;
+
+
+
+            
+            default:
+            $response['error'] = true; 
             $response['message'] = 'Invalid operation called'; 
-    	}
+        }
     }
 
     else{
-    		$response['error'] = true; 
+            $response['error'] = true; 
             $response['message'] = 'Invalid API call'; 
     }
 
 
     echo json_encode($response);
     function checkIfAllParametersAreTrue($params){
-    	foreach($params as $param){
-    		if(!isset($_POST[$param])){
-    			return false; 
-    		}
+        foreach($params as $param){
+            if(!isset($_POST[$param])){
+                return false; 
+            }
 
-    	}
-    	return true; //only if all are true 
+        }
+        return true; //only if all are true 
     }
 
 
@@ -106,7 +150,7 @@
 
 
 
-	?>
+    ?>
 </head>
 <body>
 
